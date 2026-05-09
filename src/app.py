8aba8d3ee5ec22ff6dd1f2958afe8a9c0e13102a
yisusrock <rockym0242@gmail.com>
2026-05-09 02:52:17 +0000

    Add 2 more sports related activities, 2 more artistic activities, and 2 more intellectual activities.

--- src/app.py
+++ src/app.py
@@ -20,7 +20,8 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
 
 # In-memory activity database
-activities = {
+def new_func():
+    activities = {
     "Chess Club": {
         "description": "Learn strategies and compete in chess tournaments",
         "schedule": "Fridays, 3:30 PM - 5:00 PM",
@@ -40,6 +41,10 @@ activities = {
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
     }
 }
+    
+    return activities
+
+activities = new_func()
 
 
 @app.get("/")
@@ -61,7 +66,7 @@ def signup_for_activity(activity_name: str, email: str):
 
     # Get the specific activity
     activity = activities[activity_name]
-    
+
     # Validate student is not already signed up
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already signed up")
